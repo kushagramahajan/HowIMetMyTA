1d755ee4ca3bd8b83cc6a170042687b274015ab1
Prankul Singh <prankulsinghps@gmail.com>
2016-11-29 05:23:57 +0530

    misc UI

--- app/src/main/java/com/example/kushagra/meetupapp/navDrawer/CommonCoursesListActivity.java
+++ app/src/main/java/com/example/kushagra/meetupapp/navDrawer/CommonCoursesListActivity.java
@@ -345,7 +345,6 @@ public class CommonCoursesListActivity extends AppCompatActivity
         else if (id == R.id.nav_student_side)
         {
             setAndPopulateStudentCourses();
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
